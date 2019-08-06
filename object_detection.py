47003960bbe8a68ace8f4e2986a07503747d9288
lloydagola <lloydagola@gmail.com>
2019-08-06 10:45:03 +0600

    Real time object tracking from video using HSV values

--- object_detection.py
+++ object_detection.py
@@ -4,18 +4,20 @@ import numpy as np
 def nothing(x):
     pass
 
+video_feed = cv2.cv2.VideoCapture(0)
+
 cv2.cv2.namedWindow("Tracking")
 cv2.cv2.createTrackbar("LowerHueValue", "Tracking", 0, 255, nothing)
-cv2.cv2.createTrackbar("LowerSaturationValue", "Tracking", 0, 255, nothing)
-cv2.cv2.createTrackbar("LowerValue", "Tracking", 0, 255, nothing)
 cv2.cv2.createTrackbar("UpperHueValue", "Tracking", 255, 255, nothing)
+cv2.cv2.createTrackbar("LowerSaturationValue", "Tracking", 0, 255, nothing)
 cv2.cv2.createTrackbar("UpperSaturationValue", "Tracking", 255, 255, nothing)
+cv2.cv2.createTrackbar("LowerValue", "Tracking", 0, 255, nothing)
 cv2.cv2.createTrackbar("UpperValue", "Tracking", 255, 255, nothing)
 
 while True:
-    picture = cv2.cv2.imread('colored_balls.jpg')
+    _, frame = video_feed.read()
 
-    hsv_picture = cv2.cv2.cvtColor(picture, cv2.cv2.COLOR_BGR2HSV)
+    hsv_frame = cv2.cv2.cvtColor(frame, cv2.cv2.COLOR_BGR2HSV)
 
     lower_hue = cv2.cv2.getTrackbarPos("LowerHueValue", "Tracking")
     lower_saturation = cv2.cv2.getTrackbarPos("LowerSaturationValue", "Tracking")
@@ -28,11 +30,11 @@ while True:
     lower_blue_limit = np.array([lower_hue, lower_saturation, lower_value])
     upper_blue_limit = np.array([upper_hue, upper_saturation, upper_value])
 
-    mask = cv2.cv2.inRange(hsv_picture, lower_blue_limit, upper_blue_limit)
+    mask = cv2.cv2.inRange(hsv_frame, lower_blue_limit, upper_blue_limit)
 
-    result = cv2.cv2.bitwise_and(picture, picture, mask=mask)
+    result = cv2.cv2.bitwise_and(frame, frame, mask=mask)
 
-    cv2.cv2.imshow("picture", picture)
+    cv2.cv2.imshow("frame", frame)
     cv2.cv2.imshow("mask", mask)
     cv2.cv2.imshow("result", result)
 
@@ -40,4 +42,5 @@ while True:
     if key == 27:
         break
 
+video_feed.release()
 cv2.cv2.destroyAllWindows()
