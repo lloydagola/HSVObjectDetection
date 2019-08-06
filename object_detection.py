6df83578618e8cc0867acc9b37f761fa00cfd1f7
lloydagola <lloydagola@gmail.com>
2019-08-06 10:17:48 +0600

    Added trackbars to allow user to modify hsv values at runtime

--- object_detection.py
+++ object_detection.py
@@ -1,28 +1,43 @@
-import cv2
+import cv2.cv2
 import numpy as np
 
 def nothing(x):
     pass
 
+cv2.cv2.namedWindow("Tracking")
+cv2.cv2.createTrackbar("LowerHueValue", "Tracking", 0, 255, nothing)
+cv2.cv2.createTrackbar("LowerSaturationValue", "Tracking", 0, 255, nothing)
+cv2.cv2.createTrackbar("LowerValue", "Tracking", 0, 255, nothing)
+cv2.cv2.createTrackbar("UpperHueValue", "Tracking", 255, 255, nothing)
+cv2.cv2.createTrackbar("UpperSaturationValue", "Tracking", 255, 255, nothing)
+cv2.cv2.createTrackbar("UpperValue", "Tracking", 255, 255, nothing)
+
 while True:
-    picture = cv2.cv2.imread("colored_balls.jpg")
+    picture = cv2.cv2.imread('colored_balls.jpg')
 
     hsv_picture = cv2.cv2.cvtColor(picture, cv2.cv2.COLOR_BGR2HSV)
 
-    lower_blue_limit = np.array([110, 50, 50])
-    upper_blue_limit = np.array([130, 255, 255])
+    lower_hue = cv2.cv2.getTrackbarPos("LowerHueValue", "Tracking")
+    lower_saturation = cv2.cv2.getTrackbarPos("LowerSaturationValue", "Tracking")
+    lower_value = cv2.cv2.getTrackbarPos("LowerValue", "Tracking")
+
+    upper_hue = cv2.cv2.getTrackbarPos("UpperHueValue", "Tracking")
+    upper_saturation = cv2.cv2.getTrackbarPos("UpperSaturationValue", "Tracking")
+    upper_value = cv2.cv2.getTrackbarPos("UpperValue", "Tracking")
+
+    lower_blue_limit = np.array([lower_hue, lower_saturation, lower_value])
+    upper_blue_limit = np.array([upper_hue, upper_saturation, upper_value])
 
     mask = cv2.cv2.inRange(hsv_picture, lower_blue_limit, upper_blue_limit)
 
     result = cv2.cv2.bitwise_and(picture, picture, mask=mask)
 
-    cv2.cv2.imshow("Object Detection", picture)
-    cv2.cv2.imshow("Mask", mask)
-    cv2.cv2.imshow("Result", result)
+    cv2.cv2.imshow("picture", picture)
+    cv2.cv2.imshow("mask", mask)
+    cv2.cv2.imshow("result", result)
 
-    if cv2.cv2.waitKey(0) & 0xFF == 27:
+    key = cv2.cv2.waitKey(1)
+    if key == 27:
         break
 
 cv2.cv2.destroyAllWindows()
-
-
